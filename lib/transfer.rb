b3a96fae5f826d250f3402165e9f05e3d40fadad
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-04-05 16:40:56 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,15 +13,16 @@ class Transfer
   end
 
   def execute_transaction
-    if @status == "pending" && @sender.balance > @amount
+    if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
       @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      return "Transaction rejected. Please check your account balance."
     end
   end
+
 
   def reverse_transfer
 
